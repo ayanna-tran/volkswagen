5b6728600abbc64932e796e4c42b12ee38cdeef9
minhthao <minhthaon2@gmail.com>
2020-11-07 15:22:32 +0700

    split file

--- index.php
+++ index.php
@@ -23,13 +23,7 @@
 </head>
 
 <body>
-  <header>
-    <h1 id="logo">
-      <a href="/">
-        <img src="./images/logo-volksgawen.png" alt="Volkwagen Bình Dương - Duy 090.734.33.71"/>
-      </a>
-    </h1>
-  </header>
+  <?php include 'header.php';?>
   <main>
     <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
@@ -138,34 +132,7 @@
 
     <div class="product-list"></div>
   </main>
-  <footer class="bg-blue-dark">
-    <div class="menu-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 d-md-none d-lg-block">
-            <h3>FANPAGE FACEBOOK</h3>
-            <div class="fb-page" data-href="https://www.facebook.com/Duy_Volkswagen-B%C3%ACnh-D%C6%B0%C6%A1ng-112733500605310/" data-tabs="timeline" data-width="200" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Duy_Volkswagen-B%C3%ACnh-D%C6%B0%C6%A1ng-112733500605310/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Duy_Volkswagen-B%C3%ACnh-D%C6%B0%C6%A1ng-112733500605310/">Duy_Volkswagen Bình Dương</a></blockquote></div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <h3>THÔNG TIN LIÊN HỆ</h3>
-            <strong>ĐẠI LÝ Ô TÔ VOLKSWAGEN BÌNH DƯƠNG</strong><br/>
-            <strong>Trụ sở chính: 7A/12A Đại lộ Bình Dương, khu phố Bình Đức 1, Thuận An, Bình Dương</strong><br/>
-            <strong>Hotline: 090 734 33 71</strong><br/>
-            <strong>Email: duy.trannhat@vwbinhduong.vn</strong>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <h3>BẢN ĐỒ</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.771425780739!2d106.71060221480184!3d10.904968292234386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x92e432e129027ffb!2zVlcgQsOsbmggRMawxqFuZyAtIMSQ4bqhaSBMw70gWGUgQ2jDrW5oIEjDo25nIFZvbGtzd2FnZW4gVmlldE5hbQ!5e0!3m2!1sen!2s!4v1604629052529!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="inner-footer text-center">Copyright © 2020 VOLKSWAGEN BÌNH DƯƠNG</div>
-    </div>
-  </footer>
+  <?php include 'footer.php';?>
 </body>
-<!-- facebook -->
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v8.0&appId=394671930882478&autoLogAppEvents=1" nonce="nGJMZyJE"></script>
+<?php include 'configScript.php';?>
 </html>
